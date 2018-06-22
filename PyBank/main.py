9bf88afadbaab47000939199f22571b51da781af
Clint Goodman <goodmanc528@gmail.com>
2018-06-22 00:14:23 -0500

    Updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,25 +6,29 @@ with open('budget_data.csv','r') as csv_file:
     dates = []
     revenues = []
     
-    #next(read_csv)
+    next(read_csv)
 
     for row in read_csv:
         date = row[0]
         revenue = row[1]
 
         dates.append(date)
-        revenues.append(revenue)
+        revenues.append(int(revenue))
     
     # print(dates)
     # print(revenues)
 
-print("")
-print("Financial Analysis")
-print("")
-print("----------------------------")
+print('')
+print('Financial Analysis\n')
+print('----------------------------\n')
 
 #determine Total Months
 totalMonths = len(dates)
-print("Total Months: " + str(totalMonths))
+print('Total Months: ' + str(totalMonths) + '\n')
 
-#calc
+#calculate total net amount of Profit/Losses over entire period
+netAmount = 0
+for i in range(len(revenues)):
+    netAmount += revenues[i]
+print('Total: ' + str(netAmount) + '\n') 
+#rint('Total: ',sum(revenues))
