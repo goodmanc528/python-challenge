eb39ff4b28cc30b58d1075167f402fa0cccf9e2a
Clint Goodman <goodmanc528@gmail.com>
2018-06-23 15:59:00 -0500

    update June 23 4:00 PM

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ with open('budget_data.csv','r') as csv_file:
     
     dates = []
     revenues = []
+    profitChanges = []
     
     next(read_csv)
 
@@ -30,7 +31,28 @@ print('Total Months: ' + str(totalMonths) + '\n')
 netAmount = 0
 for i in range(len(revenues)):
     netAmount += revenues[i]
-print('Total: ' + str(netAmount) + '\n') 
+print('Total Profit/Loss: ' + str(netAmount) + '\n') 
 #print('Total: ',sum(revenues))
 
 #calculate average change between months i.e. Nov12-Oct12=month to month change
+ 
+counter = 0
+
+for j in range(len(revenues)):
+    if j == 0:
+        pass
+    else:
+        k=j-1
+        ####print(f"This is {dates[k]}'s revenue {revenues[k]}")
+        ####print(f"This is {dates[j]}'s revenue {revenues[j]}")
+        profitChanges.append(revenues[j]-revenues[k])
+        counter += 1
+
+###print(sum(profitChanges))
+###print(counter)
+print(f'Average Change: $ {sum(profitChanges)/counter}')
+
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in losses (date and amount) over the entire period
