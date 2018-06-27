c3f445045dccfccd59e74d65a746414b5a09a5c5
Clint Goodman <goodmanc528@gmail.com>
2018-06-26 22:18:10 -0500

    Update 26 June 2018, Fix Output/Input

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,19 +1,17 @@
 import csv
 import os
 import sys
-import logging
 
 def calcNumVote(candidate):
-    #perVotesfor = 0.0
+    #calculate the number of votes received for the candidate that was passed to the function
     numVotesfor = 0
     for x in candidates:
         if candidate == x:
             numVotesfor += 1      
-    #perVotesfor = numVotesfor/totalVotesCast
-    #return (perVotesfor, numVotesfor)
     return numVotesfor
 
 def calcPerVote(candidate):
+    #calculate the percent of votes recieved for the candidate that was passed to the function
     perVotesfor = 0.0
     numVotesfor = 0
     for x in candidates:
@@ -22,10 +20,6 @@ def calcPerVote(candidate):
     perVotesfor = round(((numVotesfor/totalVotesCast)*100), 3)
     return perVotesfor
     
-####return multiple values use Tuple
-####build tuple with parentheses, i.e. return TupleName
-####
-
 totalVotesCast = 0
 candidates = []
 counter = 0
@@ -39,35 +33,49 @@ with open(filepath, newline='', encoding='utf8') as csv_file:
         candidates.append(i[2])
         
     setCandidates = set(candidates)
-    ListCandidates = list(setCandidates)
 
+file_out = open('ElectionResults.txt', 'w')
 
-print('\n')
-print('Election Results\n')
-print('----------------------------\n')
+file_out.writelines(
+    [
+        'Election Results\n',
+        '----------------------------\n \n'
+    ]
+)
 
 
 # The total number of votes cast
 totalVotesCast = len(candidates)
-print(f'Total Votes Case: {totalVotesCast}\n')
-print('----------------------------\n')
+file_out.writelines(
+    [
+        f'Total Votes Case: {totalVotesCast}\n',
+        '----------------------------\n \n'
+    ]
+)
 # A complete list of candidates who received votes
 # The percentage of votes each candidate won
 # The total number of votes each candidate won
 
+#Create Set of candidates to pull unique values out and alphabetize the list
 setCandidates = sorted(set(candidates))
-ListCandidates = list(setCandidates)
-#print(ListCandidates)
-#input('Press Enter....')
 
 mostVotes = 0.0
 
-for i in ListCandidates:
-    print(f'{i}: {calcPerVote(i)}% ({calcNumVote(i)}) \n')
+for i in setCandidates:
+    file_out.write(f'{i}: {calcPerVote(i)}% ({calcNumVote(i)}) \n \n')
     if calcPerVote(i) > mostVotes:
         winner = i
         mostVotes = calcPerVote(i)
 
 # The winner of the election based on popular vote.
-print('----------------------------\n')
-print(f'Winner: {winner}')
+file_out.writelines(
+    [
+        '----------------------------\n',
+        f'Winner: {winner}'
+    ]
+)
+file_out.close()
+
+file_in = open('ElectionResults.txt', 'r')
+print(file_in.read())
+file_in.close()
